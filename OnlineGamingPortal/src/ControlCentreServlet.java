8843b73c7ca41f78028bfc0aee937ba4f105cc1f
buije ohadike <c23505126@mytudublin.ie>
2025-11-02 18:46:23 +0000

    Added Credit functions.

--- OnlineGamingPortal/src/ControlCentreServlet.java
+++ OnlineGamingPortal/src/ControlCentreServlet.java
@@ -1,5 +1,5 @@
 
-
+import java.sql.ResultSet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -15,46 +15,84 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ControlCentreServlet extends HttpServlet{
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws 
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws 
 	ServletException, IOException{
 
 		//1. Get the user's input from the HTML Page
 		String usernameInputByUser = request.getParameter("userName");
 		String passwordInputByUser = request.getParameter("passWord");
 		 String passwordInputByUser2 = request.getParameter("passWord2");
+		
+		 
+		 
+		 if (passwordInputByUser.equals(passwordInputByUser2)) {
+			 Connection connection = null;
+				try {
+					connection = DriverManager.getConnection(
+							"jdbc:mysql://localhost:3306/GamingPortal?serverTimezone=UTC", "root", "root");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-		//2 Put the user info in the database
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/GamingPortal?serverTimezone=UTC","root", "root");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+				try {
+					PreparedStatement stmt = connection.prepareStatement(
+							"INSERT into User (userName,passWord,Credits)" +" VALUES (?,?,?)");
+					//pass in the values as parameters
+					stmt.setString(1, usernameInputByUser);
+					stmt.setString(2, passwordInputByUser);
+					stmt.setInt(3, 500); 
+					stmt.executeUpdate();
+					stmt.close();
+					
+					
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
 
-		try {
-			PreparedStatement enterMood = connection.prepareStatement(
-					"INSERT into Users "
-							+ "(userName,passWord)" +" VALUES (?,?)");
-			//pass in the values as parameters
-			enterMood.setString(1, usernameInputByUser);
-			enterMood.setString(2, passwordInputByUser);
-			int rowsUpdated = enterMood.executeUpdate();
-			enterMood.close();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
+				String sql = "SELECT * FROM User WHERE userName='" + usernameInputByUser;
+				
+				
+				int CreditsInputByUser = 0;
+				try {
+				PreparedStatement stmt = connection.prepareStatement(sql);
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					CreditsInputByUser = rs.getInt("Credits");
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+				
 
-		//3. Send back a response to the user
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>Response Page</title></head><body> Hello User. "
-				+  "User Info is :"+usernameInputByUser+ passwordInputByUser+passwordInputByUser2+
-                ".</br><h1> THANK YOU </h1> </body><html>");
+				//3. Send back a response to the user
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				out.println("<html><head><title>Response Page</title></head><body> Hello User. "
+						+  "\t\nUserName :"+usernameInputByUser+"\t\nPassWord:  "+ passwordInputByUser+"\t\nCredits:  "+ CreditsInputByUser+
+		                ".</br><h1> THANK YOU </h1> </body><html>");
+        
+         }
+		 else{
+			 response.setContentType("text/html");
+			 PrintWriter out = response.getWriter();
+			 out.println("<html><head><title>Response Page</title></head><body> Passwords do not match. "
+                        +  "\t\nUserName :"+usernameInputByUser+"\t\nPassWord:  "+ passwordInputByUser+
+                        ".</br><h1> PLEASE TRY AGAIN </h1> </body><html>");
+			 
+			 
+			 
+			 
+			 
+			 
+		 }
+		 
 	}
-
 }
+		 
+		 
 
 	
 
